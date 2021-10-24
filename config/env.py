03564bce9e6cb91f42e401023be080149f1cf6ad
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-10-24 23:44:15 +0300

    Update env.py

--- config/env.py
+++ config/env.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv('BOT_TOKEN')
-MONGO_URI = getenv('MONGO_URI')
-SUDO_USERS = list(map(int, getenv('SUDO_USERS').split()))
+BOT_TOKEN = getenv('2056354715:AAF-KB6XzgYF4tdDf_5W_2UU-KOs5pS6-cE')
+MONGO_URI = getenv('mongodb://mongo:wNmRjCTKL6WkYQGwd5aw@containers-us-west-17.railway.app:6814')
+SUDO_USERS = list(map(int, getenv('B0RZ7').split()))
