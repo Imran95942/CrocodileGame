88f854283393b8c2f85553e06bf13923301491e8
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-10-25 00:54:00 +0300

    Update env.py

--- config/env.py
+++ config/env.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 BOT_TOKEN = getenv('2056354715:AAF-KB6XzgYF4tdDf_5W_2UU-KOs5pS6-cE')
 MONGO_URI = getenv('mongodb://mongo:wNmRjCTKL6WkYQGwd5aw@containers-us-west-17.railway.app:6814')
-SUDO_USERS = list(map(int, getenv('@isIam07').split()))
+SUDO_USERS = '2091572563'
