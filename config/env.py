2993c658d601337b63e03cf894b91a44141dbab6
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-12-18 08:04:31 +0300

    Update env.py

--- config/env.py
+++ config/env.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv('BOT_TOKEN')
+BOT_TOKEN = getenv('1951787106:AAFbVL0eaJha8luz8IjPa8xBaftpoztWBU4')
 MONGO_URI = getenv('MONGO_URI')
-SUDO_USERS = list(map(int, getenv('SUDO_USERS').split()))
+SUDO_USERS = list(map(int, getenv('isIam07').split()))
