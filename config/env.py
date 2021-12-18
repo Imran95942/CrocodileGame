7efcb5997778e3206cce659758424f5a40580320
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-12-18 08:09:03 +0300

    Update env.py

--- config/env.py
+++ config/env.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv('2056354715:AAF-KB6XzgYF4tdDf_5W_2UU-KOs5pS6-cE')
+BOT_TOKEN = getenv('1951787106:AAFbVL0eaJha8luz8IjPa8xBaftpoztWBU4')
 MONGO_URI = getenv('mongodb://mongo:4Ev9MAsHe0Thh6xJwzrR@containers-us-west-17.railway.app:5638')
-SUDO_USERS = '2091572563'
+SUDO_USERS = '1901738017'
